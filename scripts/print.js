289754eb1cce9ebbbd4824da6660608d3e89c89b
piecioshka <piecioshka@gmail.com>
2013-12-05 14:49:56 +0100

    init import

---
+++ scripts/print.js
@@ -0,0 +1,28 @@
+/**
+ * @author Piotr Kowalski <piecioshka@gmail.com>
+ * @fileOverview Set style for CSS print on http://github.com/**
+ */
+(function () {
+    'use strict';
+
+    var PRINT_STYLE_FILE = chrome.extension.getURL("styles/print.css");
+
+    function createLinkTag() {
+        var tag = document.createElement("link");
+        tag.setAttribute("type", "text/css");
+        tag.setAttribute("rel", "stylesheet");
+        // tag.setAttribute("media", "print");
+        return tag;
+    }
+
+    function applyPrintStyle() {
+        var style = createLinkTag();
+        style.setAttribute("href", PRINT_STYLE_FILE);
+        document.getElementsByTagName("head")[0].appendChild(style);
+    }
+
+    // GO GO GO!
+    applyPrintStyle();
+
+}());
+
